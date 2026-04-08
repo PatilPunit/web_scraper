27fd373f89c010a7d27d31cf460c72cb20da94e2
PatilPunit <punitpatil770@gmail.com>
2026-04-08 22:34:44 +0530

    table

--- webscrap.py
+++ webscrap.py
@@ -1,7 +1,6 @@
 #All Libraries
-import pandas as pd #panda for dat handlind and dataframing 
+import pandas as pd #panda for dat handling and dataframing 
 import numpy as np #For Handling nan value
-# import math
 import matplotlib.pyplot as pl #For data visualization
 from rich.console import Console # For printing console
 from rich.table import Table #for creating table
@@ -10,6 +9,13 @@ from rich import print #for crafting font
 from rich.progress import track #for animation
 import time #tacking time
 
+def clean_data(sr=pd.Series):
+   sr.dropna
+   sr.fillna(0,inplace=True)
+   sr.drop_duplicates()
+   return sr
+
+   
 #selection function
 def select():
    l=[17,18,19,20,21,22,23]    #list of years
@@ -25,7 +31,8 @@ def select():
 
    link = "https://www.football-data.co.uk/mmz4281/" + year+"/"+event_select+"."+"csv"  #link which acces and bring the data
 
-   df=pd.read_csv(link) #reading csv file
+   df=pd.read_csv(link) #reading csv file\
+   clean_data(df)
 
    for i in track(range(20),description="Analysing Data"): time.sleep(0.1) #that loading bar animation
    print(df)
@@ -38,7 +45,8 @@ def select():
         if n==1 :
           print(emoji.emojize(":house:"),"[bold]show top home teams of full time [/bold]")   #showing the home team has best score at full time
 
-          home_goals=df[["HomeTeam","FTHG"]].copy()  
+          home_goals=df[["HomeTeam","FTHG"]].copy()
+          clean_data(home_goals) 
           home_goals.rename(columns={"FTHG":"Goals"},inplace=True)
           home_stats=home_goals.groupby("HomeTeam")  ["Goals"].sum()
           
@@ -59,6 +67,7 @@ def select():
           print(emoji.emojize(":stadium:"),"[bold]show top away teams of full time[/bold]")   #showing the away team has best score at full time 
 
           away_goals=df[["AwayTeam","FTAG"]].copy()
+          clean_data(away_goals)
           away_goals.rename(columns={"FTAG":"Goals"},inplace=True)
           away_stats=away_goals.groupby("AwayTeam")  ["Goals"].sum()
           
@@ -80,10 +89,12 @@ def select():
           print(emoji.emojize(":rocket:"),"[bold]Best Team by Result[/bold]")
 
           home_team =  df[["HomeTeam","FTR"]].copy()  #best team and thier results
+          clean_data(home_team)
           home_team["result"]=home_team["FTR"].map({'H':'W','D':'D','A':'L'}) #maping results
           home_team.rename(columns={"HomeTeam":"Team"},inplace=True)
 
           away_team =  df[["AwayTeam","FTR"]].copy()
+          clean_data(away_team)
           away_team["result"]=away_team["FTR"].map({'H':'L','D':'D','A':'W'})
           away_team=away_team.rename(columns={"AwayTeam":"Team"},inplace=True)
  
@@ -108,10 +119,12 @@ def select():
           print(emoji.emojize(":trophy:"),"[bold]Best Team by Winrate[/bold]")
 
           home_teams =  df[["HomeTeam","FTR"]].copy() #best team which has best winning rate
+          clean_data(home_teams)
           home_teams["result"]=home_teams["FTR"].map({'H':'W','D':'D','A':'L'})
           home_teams.rename(columns={"HomeTeam":"Team"},inplace=True)
 
           away_teams =  df[["AwayTeam","FTR"]].copy()
+          clean_data(away_teams)
           away_teams["result"]=away_teams["FTR"].map({'H':'L','D':'D','A':'W'})
           away_teams=away_teams.rename(columns={"AwayTeam":"Team"},inplace=True)
  
@@ -139,10 +152,12 @@ def select():
           print(emoji.emojize(":soccer_ball:"),"[bold]Best Team by Goals[/bold]")
 
           home_g = df[["HomeTeam","FTHG"]].copy() #best away and home team by goals
+          clean_data(home_g)
           home_g= home_g.rename(columns={'FTHG':'Goal',"HomeTeam":"Team"})
           home_g=home_g.groupby('Team') ['Goal'].sum()
 
           away_g = df[["AwayTeam","FTAG"]].copy()
+          clean_data(away_g)
           away_g = away_g.rename(columns={'FTAG':'Goal',"AwayTeam":"Team"})
           away_g=away_g.groupby('Team') ['Goal'].sum()
          
@@ -165,6 +180,7 @@ def select():
            print(emoji.emojize(":gun:"),"[bold]Best Team by Shotrate[/bold]")
 
            hs_team = df[["HS","HomeTeam","FTHG"]].copy() #Best team which has shotrate
+           clean_data(hs_team)
            hs_team.rename(columns={"HS":"TeamShots","HomeTeam":"Team","FTHG":"Goals"},inplace=True)
            hs_sr = hs_team.groupby("Team").sum()
            hs_sr['Home_ShotRate']=hs_sr['Goals']/hs_sr['TeamShots']
@@ -173,6 +189,7 @@ def select():
            print(sort6b)
 
            as_team = df[["AS","AwayTeam","FTAG"]].copy()
+           clean_data(as_team)
            as_team.rename(columns={"AS":"TeamShots","AwayTeam":"Team","FTAG":"Goals"},inplace=True)
            as_sr = as_team.groupby("Team").sum()
            as_sr["Away_Shotrate"]=as_sr['Goals']/as_sr['TeamShots']
@@ -200,11 +217,13 @@ def select():
            print(emoji.emojize(":Shield:"),"[bold]Show Best defending Team[/bold]")
 
            hd_team=df[["FTAG","HomeTeam"]].copy() 
+           clean_data(hd_team)
            hd_team.columns=["Goals","Team"]
            hd_df=hd_team.groupby("Team") ["Goals"].sum()
            print(hd_df)
 
            ad_team=df[["FTHG","AwayTeam"]].copy()
+           clean_data(ad_team)
            ad_team.columns=["Goals","Team"]
            ad_df=ad_team.groupby("Team") ["Goals"].sum()
            print(ad_df)
@@ -260,22 +279,24 @@ def select():
 
           
     
-
-          print("Team : ",teamnum)
-          print("Total Goals : ",t_goals)
-          print("Winrate : ",round(winr,2))
-          print("Shotrate : ",round(t_shotrate,2))
-          print("Taken Goals : ",h_def)
-          print("Toatal Wins : ",wins)
-          print("Wins : ", h_win)
+          print("------------------------------------------------------------------")
+          print("Team : ",teamnum,emoji.emojize(":group:"))
+          print("Total Goals : ",t_goals,emoji.emojize(":soccer:"))
+          print("Winrate : ",round(winr,2),emoji.emojize(":crown:"))
+          print("Shotrate : ",round(t_shotrate,2),emoji.emojize(":fire:"))
+          print("Taken Goals : ",h_def,emoji.emojize(":shield:"))
+          
+          print("Total Wins : ", h_win)
           print("Total lose : ",h_lose)
           print("Total Draws : ",h_draw)
+          print("------------------------------------------------------------------")
           
 
 
 
         elif n == 8 :
            win_h=df[['FTR','HomeTeam']].copy()
+           clean_data(win_h)
            win_h.columns=['Result','Team']
            win_h['Points']=win_h['Result'].map({'H':3,'D':1,'A':0})
 
@@ -287,16 +308,28 @@ def select():
            all_points= all_win.groupby('Team') ['Points'].sum().sort_values(ascending=False)
            top_t8=all_points.index[0]
 
-           print(f"[bold cyan]{top_t8}  is the Champion Team. We can see thier prgress throughout season making the patter of victory[/bold cyan]",emoji.emojize(":Crown:"))
+           
 
            for i in track(range(20),description="AND THE CHAMPION IS ......"): time.sleep(0.05) 
-           print(f"[bold cyan]{top_t8}  is the Champion Team. We can see thier prgress throughout season making the patter of victory[/bold cyan]",emoji.emojize(":Crown:"))
+           print(f"[bold cyan]{top_t8}  is the Champion Team. We can see thier prgress throughout season making the pattern of victory[/bold cyan]",emoji.emojize(":Crown:"))
            print(emoji.emojize(":Soccer:"))
            print("------------------------------------------------------------------")
            print(emoji.emojize(":Crown:"),"SO THE CHAMPION IS ",all_points.head(1))
            print("------------------------------------------------------------------")
-          
-        
+
+        elif n == 10:
+           h=df[['HomeTeam','FTR']].copy()
+           a=df[['AwayTeam','FTR']].copy()
+           h.columns=['Team','R']
+           a.columns=['Team','R']
+           h['P']=h["R"].map({'H':'W','D':'D','A':'L'})
+           a['P']=a["R"].map({'H':'L','D':'D','A':'W'})
+
+         #   t=pd.concat([h[['Team','R']],a[['Team','R']]])
+           t=pd.concat([h,a])
+           am=t.groupby('Team') ['P'].value_counts().unstack(fill_value=0)
+           print(am.sort_values(by="W",ascending=False))
+              
 def show_menu():
    console = Console()
 
@@ -313,6 +346,9 @@ def show_menu():
    table.add_row("6","Show Best Attacking Team")
    table.add_row("7","Show Best Defending Team")
    table.add_row("8","Show The Champion Team")
+   table.add_row("9","Choose Team to see stats")
+   table.add_row("10","Show whole Table of Teams")
+
 
    console.print(table)
                    
