2aed375585bd3c0e95f025b71f07bee9ad029ab7
PatilPunit <punitpatil770@gmail.com>
2026-08-24 00:14:55 +0530

    hometam logic

--- backend/hometeam_goal.py
+++ backend/hometeam_goal.py
@@ -1,6 +1,29 @@
 import pandas as pd
-from webscrap import df
 from data_clean import clean_data
-
+from main import df
+import matplotlib.pyplot as pl
 cleaned_df = clean_data(df)
 print(cleaned_df.head())
+
+print("[bold]show top home teams of full time [/bold]")   #showing the home team has best score at full time
+
+
+
+
+home_goals=df[["HomeTeam","FTHG"]].copy()
+clean_data(home_goals) 
+home_goals.rename(columns={"FTHG":"Goals"},inplace=True)
+home_stats=home_goals.groupby("HomeTeam")  ["Goals"].sum()
+
+print(home_stats.sort_values(ascending=False))
+sort=home_stats.sort_values(ascending=False)
+top_team=sort.index[0]
+print(f"[bold cyan]{top_team} is the Most Consistent in Home . [/bold cyan]")
+
+sort.plot(kind="bar")
+pl.xlabel("Teams")
+pl.ylabel("Goals")
+pl.title("Goals scord by Home Teams")
+pl.xticks(rotation=90)
+pl.plot()
+pl.show()
--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,24 @@
 import pandas as pd
 from data_clean import clean_data
+
+def select():
+    l=[17,18,19,20,21,22,23]
+    events = ['E0', 'E1', 'E2', 'E3', 'EC']
+    # print("Choose the year for csv file eg.(1718)")
+    # for i in l : print(i,i+1)            #loop of years
+    # year = input("Choose : ")
+    
+    # print("Choose the event for csv file between eg.(E1):")
+    # for i in events : print(i)             #loop of events
+    # event_select= input("Choose : ")
+
+    link = "https://www.football-data.co.uk/mmz4281/" + '1718'+"/"+'E1'+"."+"csv"  #link which acces and bring the data
+
+    global df
+    df = pd.read_csv(link)
+    print(df.head())
+
+select()
+
+
+
